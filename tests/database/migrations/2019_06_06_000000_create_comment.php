d3b1cceb8ee3351dbfb57daa11cc4a5dc27029e9
Philippe Thiers <pthiers@riskamerica.com>
2019-06-05 18:26:26 -0400

    Factory for the new models

--- tests/database/migrations/2019_06_06_000000_create_comment.php
+++ tests/database/migrations/2019_06_06_000000_create_comment.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUser extends Migration
+class CreateComment extends Migration
 {
     public function up()
     {
